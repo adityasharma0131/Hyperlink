9d0c8f08b87cd3b6f7e54b735340839a9266bb83
Aditya Suresh Sharma <adityasharma0431@gmail.com>
2025-07-04 13:57:18 +0530

    Component & Page Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-const App = () => {
+// Page components
+import Home from "./pages/Home/Home";
+import AboutApp from "./pages/AboutApp/AboutApp";
+import Blogs from "./pages/Blogs/Blogs";
+import Pricing from "./pages/Pricing/Pricing";
+import Contact from "./pages/Contact/Contact";
+
+// UI components
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
+
+function App() {
   return (
-    <div>App</div>
-  )
+    <Router>
+      <div>
+        <Navbar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about-app" element={<AboutApp />} />
+            <Route path="/blogs" element={<Blogs />} />
+            <Route path="/pricing" element={<Pricing />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
